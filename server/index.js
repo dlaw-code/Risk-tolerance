7ab5c6d80cc500d9481f88fbf4c1e7aa6937e5ea
Ahmed Ajala <70725023+dlaw-code@users.noreply.github.com>
2023-11-08 15:11:40 +0000

    new

--- server/index.js
+++ server/index.js
@@ -5,28 +5,29 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const data = require("./data");
 const Portfolio = require("./src/models/portfolio");
-const portfolioRoute = require("./src/routes/portflioRoute");
-//ROUTE IMPORTS
+const portfolioRoute = require("./src/routes/portfolioRoute");
 
 dotenv.config();
 
-//mongo db
-const dbURL ="mongodb+srv://lotoDEE:malcomx0147@cluster0.xs1zhyd.mongodb.net/ATAfrica";
+// MongoDB connection URI
+const dbURL = process.env.MONGODB_URI || "mongodb+srv://lotoDEE:malcomx0147@cluster0.xs1zhyd.mongodb.net/ATAfrica";
 
-console.log(dbURL);
-mongoose
-  .connect(dbURL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => {
-    console.log(err);
-  });
+// Connect to MongoDB
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("DB Connection Successful!"))
+  .catch(err => console.error("Error connecting to MongoDB:", err));
 
-//MIDDLE
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// API Routes
 app.use("/api/user/portfolio", portfolioRoute);
 
+// Insert portfolio data into the database
 async function insertPortfolioData() {
   try {
     // Clear existing portfolio data if needed
@@ -40,46 +41,19 @@ async function insertPortfolioData() {
   } catch (error) {
     // Handle any errors that occur during the insertion
     console.error("Error inserting portfolio data:", error);
-  } finally {
-    // Ensure that the database connection is closed, whether the insertion was successful or not
-    // mongoose.disconnect();
   }
 }
 
+// Uncomment the following line if you want to insert portfolio data
 // insertPortfolioData();
 
-const port = 5000;
+// Set the port for the server
+const port = process.env.PORT || 5000;
 
-const server = app.listen(port || 5000, () => {
-  if (process.env.NODE_ENV !== "production") {
-    console.log(`Server is running on port ${port}`);
-  }
+// Start the server
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-module.exports = app;
-module.exports = server;
-
-// IMPORTANT: To prevent this code from running immediately, it's commented out.
-
-// This function inserts portfolio data into the database
-// async function insertPortfolioData() {
-//   try {
-//     // Clear existing portfolio data if needed
-//     // await Portfolio.deleteMany({});
-
-//     // Insert new portfolio data from the 'data' variable
-//     // const portfolios = await Portfolio.create(data);
-
-//     // Log a success message when data is inserted
-//     // console.log('Portfolio data inserted successfully.');
-//   } catch (error) {
-//     // Handle any errors that occur during the insertion
-//     // console.error('Error inserting portfolio data:', error);
-//   } finally {
-//     // Ensure that the database connection is closed, whether the insertion was successful or not
-//     // mongoose.disconnect();
-//   }
-// }
-
-// Call the function to insert portfolio data
-// insertPortfolioData();
+// Export the app and server
+module.exports = { app, server };
